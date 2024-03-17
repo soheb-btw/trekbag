3b4cc85a2d5ded9058802ac526f9f2059db2d09b
Soheb <sohebattar1999@gmail.com>
2024-03-17 18:19:37 +0530

    logo changes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,3 +1,8 @@
 export default function Logo() {
-  return <img src="../../public/images/dots.png" alt="image" />
+  // return <img src="../../public/images/dots.png" alt="image" />
+  return <div style={{ display: 'flex',gap: "0.5rem" }}>
+    <span className="circle"></span>
+    <span className="circle"></span>
+    <span className="circle"></span>
+  </div>
 }
